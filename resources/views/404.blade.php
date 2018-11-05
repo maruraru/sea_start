41f5ea106a0d2ac004c73663ee4559a60a60839b
Mary <meari.kachusova@mail.ru>
2018-11-05 03:40:07 +0300

    fully updated

---
+++ resources/views/404.blade.php
@@ -0,0 +1,7 @@
+<!doctype html>
+
+<h3>Sorry, no page found on this route.</h3>
+
+@if(isset($myVar))
+    <p>{{var_dump($_ENV)}}</p>
+    @endif
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::fallback(function(){
+    $myVar = 'Some error';
+    return view('404', compact('myVar'));
+});
+
+Route::any('/login', 'Auth\LoginController@login');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
