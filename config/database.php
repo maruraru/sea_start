0c1730e96a2fe61151292f6591a3c3ad750236a0
Mary <meari.kachusova@mail.ru>
2018-11-13 23:54:30 +0300

    back to stable

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'testdb'),
+            'database' => env('DB_DATABASE', 'sea_start'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insertGetId(
+            [
+                'first_name' => 'Иван',
+                'second_name' => 'Иванов',
+                'last_name' => 'Иванович',
+                'email' => 'test1@gmail.com',
+                'phone' => '+79788888888',
+                'city' => 'Севастополь',
+                'school' => 'Школа№4',
+                'class' => '10',
+                'password' => Hash::make('123456'),
+            ]
+        );
     }
 }
